bb55d0c9f0799c8417e3762ac4f19541edecfe2f
vakuj <vakujrvi@gmail.com>
2021-12-15 22:53:15 +0100

    No real progress, issues with recursively go through th caves... Many combos missing.

--- d12/main.c
+++ d12/main.c
@@ -3,6 +3,7 @@
 #include <stdint-gcc.h>
 #include <string.h>
 #include <stdbool.h>
+#include <assert.h>
 
 #include "../common/common.h"
 
@@ -79,15 +80,123 @@ bool insert(cave_t *head, char *src, char *dst)
     }
     return false;
 }
-void print_rec(cave_t *head)
+
+cave_t *find(cave_t *head, char *src)
 {
-    printf("\n%s -> ", head->src);
+    cave_t *found;
+    if (head == NULL)
+        return NULL;
+    if (strcmp(head->src, src) == 0)
+        return head;
     for (size_t i = 0; i < head->dsts; i++)
     {
         if (head->dst[i] != NULL)
-            print_rec(head->dst[i]);
-        // printf("\n");
+        {
+            found = find(head->dst[i], src);
+            if (found != NULL)
+                return found;
+        }
     }
+    return NULL;
+}
+bool comp_cave(cave_t *c, char *str)
+{
+    return (bool)(strcmp(c->src, str) == 0);
+}
+
+void print_ends(cave_t *head, cave_t *next, char *pre)
+{
+    // assert(next != NULL);
+    if (next == NULL)
+        return;
+    cave_t *found;
+    cave_t *temp;
+    int ctr = 0;
+    char new_pre[BUFSIZE] = "";
+    if (strlen(pre) == 0)
+    {
+        strcat(pre, head->src);
+    }
+
+    strcpy(new_pre, pre);
+    strcat(new_pre, "->");
+    strcat(new_pre, next->src);
+    // if (strcmp(next->src, END) == 0)
+    // {
+    // strcat(new_pre, END);
+    printf("%s\n", new_pre);
+    // return;
+    // }
+    for (size_t i = 0; i < next->dsts; i++)
+    {
+        temp = next->dst[i];
+        if (temp->dst[i] == NULL && strcmp(temp->src, END) != 0)
+        {
+            printf("NULL at end: %s\n", temp->src);
+            found = find(head, temp->src);
+            // ctr = 0;
+            // while (found != NULL)
+            // {
+            if (found != NULL)
+            {
+                printf("Cont at found: %s\n", found->dst[0]->src);
+                print_ends(head, found, new_pre);
+            }
+            // found = find(found->dst[ctr], temp->src);
+            // ctr++;
+            // }m
+        }
+    }
+}
+
+void print_rec(cave_t *head, cave_t *next, char *pre)
+{
+    // printf("%s ", pre);
+    char new_pre[BUFSIZE] = "";
+    cave_t *found;
+    strcpy(new_pre, pre);
+    strcat(new_pre, "->");
+    if (next != NULL)
+    {
+        strcat(new_pre, next->src);
+        if (strcmp(next->src, END) == 0)
+        {
+            printf("%s\n", new_pre);
+            // return;
+        }
+    }
+    else
+    {
+        strcat(new_pre, head->src);
+        if (strcmp(head->src, END) == 0)
+            printf("%s\n", new_pre);
+    }
+    if (next != NULL)
+    {
+        for (size_t i = 0; i < next->dsts; i++)
+        {
+            if (next->dst[i] != NULL)
+            {
+                print_rec(head, next->dst[i], new_pre);
+                // if (strcmp(next->src, END) == 0)
+                found = find(head, next->src);
+                if (found != NULL && found != next)
+                    print_rec(head, found, new_pre);
+            }
+        }
+    }
+    else
+    {
+        for (size_t i = 0; i < head->dsts; i++)
+        {
+            if (head->dst[i] != NULL)
+            {
+                print_rec(head, head->dst[i], new_pre);
+            }
+        }
+    }
+
+    // printf("\n");
 }
 
 void parse_caves(void)
@@ -129,7 +238,7 @@ void parse_caves(void)
         strcpy(rbuf, caves[ctr]);
         pre = strtok(rbuf, DELIM);
         post = strtok(NULL, DELIM);
-        printf("%s - %s\n", pre, post);
+        // printf("%s - %s\n", pre, post);
 
         if (strcmp(pre, END) == 0)
             inserted = insert(head, post, END);
@@ -179,6 +288,7 @@ void parse_file(FILE *fp, void func(char *, int), bool silent)
 int main(int argc, char *argv[])
 {
     char outp[BUFSIZE] = "output";
+    char pbuf[BUFSIZE] = "";
     FILE *fp;
 
     if (argc > 3)
@@ -202,7 +312,12 @@ int main(int argc, char *argv[])
     // find_paths();
     // compact_cpair();
     print_caves();
-    print_rec(head);
+    // print_rec(head, (cave_t *)NULL, pbuf);
+    for (size_t i = 0; i < head->dsts; i++)
+    {
+        print_ends(head, head->dst[i], pbuf);
+    }
+
     // find_paths();
 
     fp = fopen(outp, "w");
