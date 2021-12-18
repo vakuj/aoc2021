693e0d76a2fbd5fd235a1129501d31ed18614e29
vakuj <vakujrvi@gmail.com>
2021-12-18 20:20:05 +0100

    D14 part 1 on the way, just need to make a larger char array...

--- d14/README.md
+++ d14/README.md
@@ -39,16 +39,16 @@ For example:
     
 The first line is the polymer template - this is the starting point of the process.
 
-The following section defines the pair insertion rules. A rule like AB -> C means that when elements A and B are immediately adjacent, element C should be inserted between them. These insertions all happen simultaneously.
+The following section defines the pair insertion rules. A rule like `AB -> C` means that when elements `A and B` are `immediately adjacent`, element `C` should be `inserted between them`. These insertions all happen simultaneously.
 
 So, starting with the polymer template NNCB, the first step simultaneously considers all three pairs:
 
 1. The first pair (NN) matches the rule NN -> C, so element C is inserted between the first N and the second N.
 1. The second pair (NC) matches the rule NC -> B, so element B is inserted between the N and the C.
 1. The third pair (CB) matches the rule CB -> H, so element H is inserted between the C and the B.
-1. Note that these pairs overlap: the second element of one pair is the first element of the next pair. Also, because all pairs are considered simultaneously, inserted elements are not considered to be part of a pair until the next step.
+1. `Note that these pairs overlap: the second element of one pair is the first element of the next pair`. Also, because all pairs are considered simultaneously, inserted elements are not considered to be part of a pair until the next step.
 
-After the first step of this process, the polymer becomes NCNBCHB.
+After the first step of this process, the polymer becomes `NCNBCHB`.
 
 Here are the results of a few steps using the above rules:
 
@@ -58,7 +58,20 @@ Here are the results of a few steps using the above rules:
     After step 3: NBBBCNCCNBBNBNBBCHBHHBCHB
     After step 4: NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB
 
-This polymer grows quickly. After step 5, it has length 97; After step 10, it has length 3073. After step 10, B occurs 1749 times, C occurs 298 times, H occurs 161 times, and N occurs 865 times; taking the quantity of the most common element (B, 1749) and subtracting the quantity of the least common element (H, 161) produces 1749 - 161 = 1588.
+This polymer grows quickly:
+* After `step 5`
+    - `length 97`; 
+* After `step 10`
+    - `length 3073`. 
+* After `step 10`, 
+    - `B` occurs `1749` times, 
+    - `C` occurs `298` times, 
+    - `H` occurs `161` times, and 
+    - `N` occurs `865` times; 
+* Taking the quantity of the 
+    - most common element `(B, 1749)` and 
+    - least common element `(H, 161)` 
+    - produces 1749 - 161 = 1588.
 
 ---
 
--- d14/main.c
+++ d14/main.c
@@ -7,6 +7,7 @@
 #include "../common/common.h"
 
 #define ARRSIZE 1024
+#define XLBUF 8186
 
 #define NEWLN "\n"
 #define SPACE " "
@@ -19,7 +20,7 @@ typedef struct
 } poly_t;
 
 poly_t poly[ARRSIZE];
-char seq[BUFSIZE];
+char seq[XLBUF];
 int added = 0;
 
 void print_line(char *line, int line_nbr)
@@ -40,44 +41,42 @@ char *opt_seq(char *oseq, char *p)
 {
     for (size_t i = 0; i < added; i++)
     {
-        printf("%3ld: %s->%s", i, poly[i].pair, p);
         if (strcmp(poly[i].pair, p) == 0)
         {
-            printf("seg\n");
-            oseq[0] = poly[i].pair[0];
-            oseq[1] = poly[i].pair[0];
-            oseq[2] = poly[i].pair[1];
-            printf("->%s\n", oseq);
-            // printf("%s\n", p);
-
+            memmove(oseq, poly[i].pair, 1);
+            memmove(oseq + 1, poly[i].opt, 1);
+            memmove(oseq + 2, poly[i].pair + 1, 1);
             return oseq;
         }
-        printf("\n");
     }
     return (char *)NULL;
 }
 
 void update_seq(int iters)
 {
-    char nseq[BUFSIZE];
+    char nseq[XLBUF];
     char p[16] = "";
-    char *op;
+    char op[16] = "";
+    char *ptr;
     int ctr = 0;
     int i = 0;
     while (ctr < iters)
     {
-        memset(nseq, 0, sizeof(char[BUFSIZE]));
+        memset(nseq, 0, sizeof(char[XLBUF]));
         i = 0;
         while (i < strlen(seq) - 1)
         {
             memmove(p, seq + i, 2);
-            printf("%s\n", p);
-            op = opt_seq(op, p);
-            if (op != NULL)
+            ptr = opt_seq(op, p);
+            if (ptr != NULL)
             {
-                // strncpy(op, , 3);
-                strcat(nseq, op);
+                if (i == 0)
+                    memmove(nseq + strlen(nseq), ptr, 3);
+                else
+                    memmove(nseq + strlen(nseq), ptr + 1, 2);
             }
+            else
+                printf("Null pointer encountered, %s not found...\n", p);
             i += 1;
         }
         strcpy(seq, nseq);
@@ -91,14 +90,12 @@ void parse_file(FILE *fp, void func(char *, int), bool silent)
     char next[BUFSIZE];
     char *ptr;
     int next_ctr = 0;
-    int ptr_ctr = 0;
 
     memset(poly, 0, sizeof(poly));
 
     while (fgets(next, BUFSIZE, fp) != NULL)
     {
         ptr = strtok(next, NEWLN);
-        // printf("ptr: %s\n", ptr);
         if (next_ctr == 0)
             strcpy(seq, ptr);
         else
@@ -106,20 +103,15 @@ void parse_file(FILE *fp, void func(char *, int), bool silent)
             ptr = strtok(ptr, DELIM);
             if (ptr != NULL)
             {
-                // printf("_%s_", ptr);
                 strcpy(poly[added].pair, ptr);
                 ptr = strtok(NULL, DELIM);
-                // printf("_%s_", ptr);
                 strcpy(poly[added].opt, ptr);
                 added++;
             }
         }
 
-        ptr_ctr = 0;
-
         next_ctr++;
     }
-    // added = next_ctr;
 }
 
 int main(int argc, char *argv[])
@@ -144,10 +136,10 @@ int main(int argc, char *argv[])
 
     parse_file(fp, print_line, true);
     fclose(fp);
-    print_input();
-    printf("%s\n", seq);
-    update_seq(1);
-    printf("%s\n", seq);
+    // print_input();
+    // printf("%s\n", seq);
+    update_seq(10);
+    printf("After: (%ld) \n%s\n", strlen(seq), seq);
 
     fp = fopen(outp, "w");
     fprintf(fp, "Part 1 not solved yet.\n");
