24ba1bb1d519d75242ced508867a621505d2b3ac
vakuj <vakujrvi@gmail.com>
2021-12-26 11:10:43 +0100

    D14 part 2 done, took a while to realize how to structure the problem. Also had some strange issues with resets of important variables, likely due to stepping outside of array bounds and accidentally resetting them...

--- d14/main.c
+++ d14/main.c
@@ -22,10 +22,13 @@ typedef struct
     char opt[16];
 } poly_t;
 
-poly_t poly[ARRSIZE];
-char seq[ARRSIZE];
-size_t counted[25];
-char poly_lut[LUTSIZE];
+poly_t poly[ARRSIZE] = {0};
+char seq[ARRSIZE] = "";
+
+size_t counted[25] = {0};
+size_t pair_ctr[25][25] = {0};
+bool inseq[25][25] = {0};
+
 int added = 0;
 
 void print_line(char *line, int line_nbr)
@@ -42,74 +45,127 @@ void print_input(void)
     }
 }
 
-void init_lut(void)
+bool is_tracked(const char *p)
 {
-    memset(poly_lut, 0, sizeof(poly_lut));
+    return inseq[(int)p[0] - 65][(int)p[1] - 65];
+}
 
+void set_tracked(bool arr[25][25], const char *p)
+{
+    arr[(int)p[0] - 65][(int)p[1] - 65] = true;
+}
+
+void add_pair_ctr(size_t arr[25][25], const char *p, const size_t cnt)
+{
+    arr[(int)p[0] - 65][(int)p[1] - 65] += cnt;
+}
+
+size_t get_pair_ctr(const size_t arr[25][25], const char *p)
+{
+    return arr[(int)p[0] - 65][(int)p[1] - 65];
+}
+
+void add_counted(const char *p, const size_t cnt)
+{
+    counted[p[0] - 'A'] += cnt;
+    counted[p[1] - 'A'] += cnt;
+}
+
+void set_counted(const char *p, const size_t cnt)
+{
+    counted[p[0] - 'A'] = cnt;
+    counted[p[1] - 'A'] = cnt;
+}
+
+void update_pair_ctr(int iters)
+{
+    char p[16] = "";
+    char opt[16] = "";
+    char np[16] = "";
+    size_t cnt = 0;
+    int i = 0;
+
+    size_t new_ctr[25][25] = {0};
+    bool new_track[25][25] = {0};
+
+    /** Set or reset to default values */
+    memset(counted, 0, sizeof(size_t[25]));
+    memset(pair_ctr, 0, sizeof(size_t[25][25]));
+    memset(inseq, 0, sizeof(bool[25][25]));
+    memset(new_ctr, 0, sizeof(size_t[25][25]));
+    memset(new_track, 0, sizeof(bool[25][25]));
+
+    /** Add initial sequence to pair counter */
+    for (size_t i = 0; i < strlen(seq) - 1; i++)
+    {
+        memmove(p, seq + i, 2);
+        set_tracked(inseq, p);
+        add_pair_ctr(pair_ctr, p, 1);
+    }
+
+    while (iters > 0)
+    {
+        i = 0;
+        while (i < added)
+        {
+            /** If pair is tracked update it */
+            if (is_tracked(poly[i].pair))
+            {
+                /** Get previous count */
+                cnt = get_pair_ctr(pair_ctr, poly[i].pair);
+                if (cnt == 0)
+                    cnt = 1;
+
+                /** Find new pairs */
+                strncpy(opt, poly[i].pair, 1);
+                strncpy(opt + 1, poly[i].opt, 1);
+                strncpy(opt + 2, poly[i].pair + 1, 1);
+
+                /** Add first pair */
+                memmove(np, opt, 2);
+                set_tracked(new_track, np);
+                add_pair_ctr(new_ctr, np, cnt);
+                /** Add second pair */
+                memmove(np, opt + 1, 2);
+                set_tracked(new_track, np);
+                add_pair_ctr(new_ctr, np, cnt);
+            }
+            i++;
+        }
+        /** Move new pairs to for next iter */
+        memmove(pair_ctr, new_ctr, sizeof(size_t[25][25]));
+        memmove(inseq, new_track, sizeof(bool[25][25]));
+        /** Reset for next iter */
+        memset(new_ctr, 0, sizeof(size_t[25][25]));
+        memset(new_track, 0, sizeof(bool[25][25]));
+
+        iters--;
+    }
+    /** Move pair counters to letter counters */
     for (size_t i = 0; i < added; i++)
     {
-        memmove(poly_lut + (poly[i].pair[0] - 65) * 24 + (poly[i].pair[1] - 65), poly[i].opt, 1);
+        cnt = get_pair_ctr(pair_ctr, poly[i].pair);
+        add_counted(poly[i].pair, cnt);
     }
-}
-
-char *opt_lut(char *oseq, char *p)
-{
-    memmove(oseq, p, 1);
-    memmove(oseq + 1, poly_lut + ((int)p[0] - 65) * 24 + ((int)p[1] - 65), 1);
-    memmove(oseq + 2, p + 1, 1);
-    return oseq;
-}
-
-void rec_count(char *p, int ctr, int end)
-{
-    if (ctr == end)
+    /** Fix counts for doublets, e.g. divide by 2
+     *  If count is not even divide by 2 and add 1, e.g. round up
+     */
+    for (size_t i = 0; i < 26; i++)
     {
-        return;
+        if (counted[i] % 2 == 0)
+            counted[i] = counted[i] / 2;
+        else
+            counted[i] = counted[i] / 2 + 1;
     }
-    ctr++;
-
-    char *oseq = (char *)calloc(0, sizeof(char) * 3);
-    char *pre = (char *)calloc(0, sizeof(char) * 2);
-    char *post = (char *)calloc(0, sizeof(char) * 2);
-
-    opt_lut(oseq, p);
-
-    counted[(int)oseq[1] - 65] += 1;
-
-    memmove(pre, oseq, 2);
-    memmove(post, oseq + 1, 2);
-    free(oseq);
-
-    rec_count(pre, ctr, end);
-    free(pre);
-
-    rec_count(post, ctr, end);
-    free(post);
 }
 
-void count_seq(int end)
-{
-    char *ptr = (char *)calloc(0, sizeof(char) * 2);
-    memset(counted, 0, sizeof(counted));
-    init_lut();
-    int i = 0;
-    while (i < strlen(seq) - 1)
-    {
-        memmove(ptr, seq + i, 2);
-        rec_count(ptr, 0, end);
-        counted[(int)ptr[1] - 65] += 1;
-        i++;
-    }
-    counted[(int)seq[0] - 65] += 1;
-    free(ptr);
-}
-
-size_t get_result(void)
+size_t get_result(bool enable_print)
 {
     size_t min = 0,
            max = 0,
+           res = 0,
            total = 0;
-    for (size_t i = 0; i < 25; i++)
+    for (size_t i = 0; i < 26; i++)
     {
         if (min == 0 && counted[i] != 0)
             min = counted[i];
@@ -121,235 +177,16 @@ size_t get_result(void)
             max = counted[i];
         total += counted[i];
     }
-    printf("--- Stats ---\n");
-    printf("Tot cnt: %ld\nMax cnt: %ld\nMin cnt: %ld\nMax-Min: %ld\n", total, max, min, max - min);
-    printf("=============\n");
+    res = max - min;
 
-    return max - min;
-}
-
-char *opt_seq(char *oseq, char *p)
-{
-    for (size_t i = 0; i < added; i++)
+    if (enable_print)
     {
-        if (strcmp(poly[i].pair, p) == 0)
-        {
-            memmove(oseq, poly[i].pair, 1);
-            memmove(oseq + 1, poly[i].opt, 1);
-            memmove(oseq + 2, poly[i].pair + 1, 1);
-            return oseq;
-        }
-    }
-    return (char *)NULL;
-}
-
-size_t count_poly(int fnbr)
-{
-    FILE *fp;
-    char fname[BUFSIZE];
-    size_t counts['Z' - 'A'];
-    int nchar;
-    size_t min = 0, max = 0, total = 0;
-    memset(counts, 0, sizeof(counts));
-
-    sprintf(fname, "%s%d", TMPFILE, fnbr);
-    fp = fopen(fname, "r");
-    nchar = fgetc(fp);
-    while (nchar != EOF)
-    {
-        counts[nchar - 'A'] += 1;
-        nchar = fgetc(fp);
-    }
-    fclose(fp);
-    for (size_t i = 0; i < ('Z' - 'A'); i++)
-    {
-        if (min == 0 && counts[i] != 0)
-            min = counts[i];
-
-        if (counts[i] < min && counts[i] != 0)
-            min = counts[i];
-
-        if (max == 0 || counts[i] > max)
-            max = counts[i];
-        total += counts[i];
-    }
-    printf("--- Stats for %s%d ---\n", TMPFILE, fnbr);
-    printf("Tot cnt: %ld\nMax cnt: %ld\nMin cnt: %ld\nMax-Min: %ld\n", total, max, min, max - min);
-    printf("=======\n");
-
-    return max - min;
-}
-
-void update_seq(int start, int stop)
-{
-    FILE *fpr, *fpw;
-    char read_file[BUFSIZE], write_file[BUFSIZE];
-    fpos_t pos;
-
-    int rwbuf = 2000000;
-    // char rbuf[rwbuf], wbuf[rwbuf];
-    char *rbuf = (char *)calloc(rwbuf, sizeof(char));
-    char *wbuf = (char *)calloc(rwbuf, sizeof(char));
-
-    init_lut();
-
-    char p[16] = "";
-    char op[16] = "";
-    char *ptr;
-    int ctr = start;
-    int rcnt = 0;
-    size_t i = 0;
-
-    /** Put init sequence to file
-     *  if start is 0 else assume
-     *  sequence already started
-     *  and file exists.
-     */
-    if (ctr == 0)
-    {
-        sprintf(read_file, "%s%d", TMPFILE, ctr);
-        fpr = fopen(read_file, "w+");
-        fwrite(seq, sizeof(char), strlen(seq), fpr);
-        fclose(fpr);
+        printf("--- Stats ---\n");
+        printf("Tot cnt: %ld\nMax cnt: %ld\nMin cnt: %ld\nMax-Min: %ld\n", total, max, min, res);
+        printf("=============\n");
     }
 
-    /** Init r/w buffers */
-    memset(wbuf, 0, sizeof(char[rwbuf]));
-    memset(rbuf, 0, sizeof(char[rwbuf]));
-
-    while (ctr < stop)
-    {
-        /** Init r/w files */
-        sprintf(read_file, "%s%d", TMPFILE, ctr);
-        sprintf(write_file, "%s%d", TMPFILE, ctr + 1);
-        fpr = fopen(read_file, "r");
-        fpw = fopen(write_file, "w+");
-        rewind(fpr);
-        rewind(fpw);
-
-        /** Read to read buffer */
-        fread(rbuf, sizeof(char), rwbuf / 2 - 3, fpr);
-        rcnt = 0;
-        while (true)
-        {
-            i = 0;
-            /** Process read buffer */
-            while (i < strlen(rbuf) - 1)
-            {
-                memmove(p, rbuf + i, 2);
-                // ptr = opt_seq(op, p);
-                ptr = opt_lut(op, p);
-
-                if (ptr != NULL)
-                {
-                    if (i == 0) // Use all three from new sequence
-                        memmove(wbuf + strlen(wbuf), ptr, 3);
-                    else // only use the last two from sequence (first already added)
-                        memmove(wbuf + strlen(wbuf), ptr + 1, 2);
-                }
-                else
-                    printf("Null pointer encountered, %s not found...\n", p);
-                i += 1;
-            }
-            /** Write write buffer to file.
-             *  Write the entire buffer on first call.
-             *  Else: Write all except 0th index.
-             */
-            if (rcnt == 0)
-            {
-                fwrite(wbuf, sizeof(char), strlen(wbuf), fpw);
-                rcnt = 1;
-            }
-            else
-                fwrite(wbuf + 1, sizeof(char), strlen(wbuf) - 1, fpw);
-
-            /** Reset r/w buffers */
-            memset(wbuf, 0, sizeof(char[rwbuf]));
-            memset(rbuf, 0, sizeof(char[rwbuf]));
-
-            /** Try read more from file */
-            if (feof(fpr) != 0)
-            {
-                break;
-            }
-            else
-            {
-                fseek(fpr, -1, SEEK_CUR);
-                fread(rbuf, sizeof(char), rwbuf / 2 - 3, fpr);
-            }
-        }
-        fgetpos(fpw, &pos);
-        /** Close files */
-        fclose(fpr);
-        fclose(fpw);
-
-        /** Prgress indicator */
-        printf("%d: %ld\n", ctr, pos.__pos);
-
-        ctr++;
-    }
-    free(wbuf);
-    free(rbuf);
-}
-
-void update_seq2(int iters)
-{
-    // char buf[8196] = "";
-    // char nseq[8196] = "";
-    // char pseq[8196] = "";
-    size_t bufsize = 200000000;
-    char *buf = (char *)calloc(bufsize, sizeof(char)),
-         *nseq = (char *)calloc(bufsize, sizeof(char)),
-         *pseq = (char *)calloc(bufsize, sizeof(char));
-
-    init_lut();
-    char p[16] = "";
-    char op[16] = "";
-    char *ptr;
-    int i = 0;
-    int j = 0;
-    int ctr = 0;
-    int offset = 0;
-
-    while (i < strlen(seq) - 1)
-    {
-        ctr = 0;
-
-        memmove(buf, seq + i, 2);
-        // printf("buf: %s -> %s\n", buf, nseq);
-        while (ctr < iters)
-        {
-            j = 0;
-            // printf("%s\n", buf);
-            while (j < strlen(buf) - 1)
-            {
-                memmove(p, buf + j, 2);
-                // ptr = opt_seq(op, p);
-                ptr = opt_lut(op, p);
-                if (ptr != NULL)
-                {
-                    if (i == 0 && j == 0)
-                        memmove(pseq, ptr, 3);
-                    else
-                        memmove(pseq + strlen(pseq), ptr + 1, 2);
-                }
-                j++;
-            }
-            memmove(buf, pseq, strlen(pseq));
-            memset(pseq, 0, sizeof(char) * bufsize);
-            printf("%d: %ld\n", ctr, strlen(buf));
-            ctr++;
-        }
-
-        memmove(nseq + strlen(nseq), buf, strlen(buf));
-        memset(buf, 0, sizeof(char) * bufsize);
-        break;
-        i++;
-    }
-    free(buf);
-    free(nseq);
-    free(pseq);
-    // printf("buf: %s -> %s\n", seq, nseq);
+    return res;
 }
 
 void parse_file(FILE *fp, void func(char *, int), bool silent)
@@ -357,7 +194,6 @@ void parse_file(FILE *fp, void func(char *, int), bool silent)
     char next[BUFSIZE];
     char *ptr;
     int next_ctr = 0;
-
     memset(poly, 0, sizeof(poly));
 
     while (fgets(next, BUFSIZE, fp) != NULL)
@@ -404,20 +240,16 @@ int main(int argc, char *argv[])
     parse_file(fp, print_line, true);
     fclose(fp);
 
-    count_seq(10);
-    size_t pt1 = get_result();
-    count_seq(40);
-    size_t pt2 = get_result();
+    update_pair_ctr(10);
+    size_t pt1 = get_result(false);
+    update_pair_ctr(40);
+    size_t pt2 = get_result(false);
 
-    for (size_t i = 0; i < 26; i++)
-    {
-        printf("%c: %ld\n", (char)(i + 65), counted[i]);
-    }
-
-    // fp = fopen(outp, "w");
-    // fprintf(fp, "Part 1:Diff between most and least: %ld\n", pt1);
-    // fprintf(fp, "Part 1:Diff between most and least: %ld\n", pt2);
-    // fclose(fp);
+    fp = fopen(outp, "w");
+    fprintf(fp, "Difference between max and min count:\n");
+    fprintf(fp, "Part 1: %ld\n", pt1);
+    fprintf(fp, "Part 2: %ld\n", pt2);
+    fclose(fp);
     printf("Done, see \"%s\" for result\n", outp);
     return 0;
 }
